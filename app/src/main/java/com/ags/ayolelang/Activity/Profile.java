71129c42dcb872f679e5ebdfca2bb574bee521f1
Gusrinda <39164366+Gusrinda@users.noreply.github.com>
2019-06-20 15:25:15 +0700

    Push 37 (Lupa Password, No Connection)

---
+++ app/src/main/java/com/ags/ayolelang/Activity/Profile.java
@@ -0,0 +1,15 @@
+package com.ags.ayolelang.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ags.ayolelang.R;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
