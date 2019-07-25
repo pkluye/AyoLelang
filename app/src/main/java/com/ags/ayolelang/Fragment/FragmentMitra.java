7569224d709b8dda3800165e5ea4b06e4bfbffa7
Gusrinda <putragusrinda.lutfi@gmail.com>
2019-07-25 19:46:07 +0700

    Push 71 (Fragment edit)

--- app/src/main/java/com/ags/ayolelang/Fragment/FragmentMitra.java
+++ app/src/main/java/com/ags/ayolelang/Fragment/FragmentMitra.java
@@ -1,171 +1,109 @@
 package com.ags.ayolelang.Fragment;
 
-import android.app.ProgressDialog;
-import android.content.Intent;
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
-import com.ags.ayolelang.API.RetrofitClient;
-import com.ags.ayolelang.Activity.MainActivity;
-import com.ags.ayolelang.Activity.MessageActivity;
-import com.ags.ayolelang.Activity.Preview;
-import com.ags.ayolelang.Activity.Profile;
-import com.ags.ayolelang.Adapter.AdapterListProgress_itemHistoriTawaran;
-import com.ags.ayolelang.DBHelper.HistoriTawaranHelper;
-import com.ags.ayolelang.Models.StringRespon;
-import com.ags.ayolelang.Models.Tawaran;
-import com.ags.ayolelang.Models.User;
 import com.ags.ayolelang.R;
-import com.ags.ayolelang.Storage.SharedPrefManager;
 
-import java.io.Serializable;
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.util.ArrayList;
-import java.util.Locale;
-
-import de.hdodenhof.circleimageview.CircleImageView;
-import io.reactivex.Single;
-import io.reactivex.SingleObserver;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
-
-import static com.ags.ayolelang.API.RetrofitClient.secret_key;
-
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link FragmentMitra.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link FragmentMitra#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class FragmentMitra extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
 
-    private TextView txt_namaMitra;
-    private TextView txt_kotaProv;
-    private Button btn_profilMitra;
-    private Button btn_messageMitra;
-    private EditText txt_penawaran_mitra;
-    private RecyclerView rv_riwayatPenawaran;
-    private Button btn_pilihMitra;
-    private CircleImageView img_Mitra;
-    private ImageButton btn_Menu;
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public FragmentMitra() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FragmentMitra.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FragmentMitra newInstance(String param1, String param2) {
+        FragmentMitra fragment = new FragmentMitra();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_fragment_mitra, null);
-        img_Mitra = view.findViewById(R.id.img_Mitra);
-        txt_namaMitra = view.findViewById(R.id.txt_namaMitra);
-        txt_kotaProv = view.findViewById(R.id.txt_kotaProv);
-        btn_profilMitra = view.findViewById(R.id.btn_profilMitra);
-        btn_messageMitra = view.findViewById(R.id.btn_messageMitra);
-        txt_penawaran_mitra = view.findViewById(R.id.txt_penawaran_mitra);
-        rv_riwayatPenawaran = view.findViewById(R.id.rv_riwayatPenawaran);
-        btn_pilihMitra = view.findViewById(R.id.btn_pilihMitra);
-        btn_Menu=view.findViewById(R.id.btn_Menu);
-
-        Bundle bundle = getArguments();
-        final User user = (User) bundle.getSerializable("user");
-        final Tawaran tawaran = (Tawaran) bundle.getSerializable("tawaran");
-
-        txt_namaMitra.setText(user.getUser_nama());
-        txt_penawaran_mitra.setText("Rp. "+currencyFormat(tawaran.getTawaran_anggaran()+""));
-
-        HistoriTawaranHelper historiTawaranHelper=new HistoriTawaranHelper(getContext());
-        historiTawaranHelper.open();
-        ArrayList<Tawaran> tawarans=historiTawaranHelper.getlisttawaran(tawaran.getTawaran_lelangid(),user.getUser_id());
-        historiTawaranHelper.close();
-        AdapterListProgress_itemHistoriTawaran adapterListProgress_itemHistoriTawaran=new AdapterListProgress_itemHistoriTawaran(getContext());
-        adapterListProgress_itemHistoriTawaran.addItem(tawarans);
-        rv_riwayatPenawaran.setAdapter(adapterListProgress_itemHistoriTawaran);
-
-        btn_profilMitra.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(getActivity(), Profile.class);
-                intent.putExtra("user",user);
-                startActivity(intent);
-            }
-        });
-        btn_messageMitra.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(getActivity(), MessageActivity.class);
-                intent.putExtra("userid2",user.getUser_id());
-                startActivity(intent);
-            }
-        });
-
-        btn_Menu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getActivity().onBackPressed();
-            }
-        });
-
-        btn_pilihMitra.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pilih_mitra(tawaran.getTawaran_lelangid(),user);
-            }
-        });
-        return view;
+        return inflater.inflate(R.layout.fragment_fragment_mitra, container, false);
     }
 
-    private void pilih_mitra(int lelang_id, User user) {
-        Single<StringRespon> single= RetrofitClient.getInstance().getApi().lelang_hire(
-                secret_key,
-                lelang_id+"",
-                SharedPrefManager.getInstance(getContext()).getUser().getUser_id(),
-                user.getUser_id()
-        );
-
-        final ProgressDialog progressDoalog;
-        progressDoalog = new ProgressDialog(getActivity());
-        //progressDoalog.setMax(100);
-        progressDoalog.setMessage("Loading....");
-        //progressDoalog.setTitle("ProgressDialog bar example");
-        progressDoalog.setCancelable(false);
-        progressDoalog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        // show it
-        progressDoalog.show();
-        single.subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new SingleObserver<StringRespon>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onSuccess(StringRespon stringRespon) {
-                        progressDoalog.dismiss();
-                        Fragment fragment=new GarapanFragment();
-                        ((MainActivity)getActivity()).loadFragment_(fragment);
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        progressDoalog.dismiss();
-                        e.printStackTrace();
-                    }
-                });
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
     }
 
-    private String currencyFormat(String harga) {
-        Locale localeID = new Locale("in", "ID");
-        harga = harga.replaceAll("[.,]", "");
-        DecimalFormat kursIndonesia = (DecimalFormat) DecimalFormat.getCurrencyInstance(localeID);
-        DecimalFormatSymbols formatRp = new DecimalFormatSymbols();
-        formatRp.setCurrencySymbol("");
-        formatRp.setGroupingSeparator('.');
-        formatRp.setMonetaryDecimalSeparator(',');
-        kursIndonesia.setDecimalFormatSymbols(formatRp);
-        return kursIndonesia.format(Double.parseDouble(harga));
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
     }
 }
--- app/src/main/java/com/ags/ayolelang/Fragment/FragmentPemilihanMitra.java
+++ app/src/main/java/com/ags/ayolelang/Fragment/FragmentPemilihanMitra.java
@@ -1,101 +1,109 @@
 package com.ags.ayolelang.Fragment;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
-import com.ags.ayolelang.Adapter.AdapterListProgress_itemMitra;
-import com.ags.ayolelang.DBHelper.LelangHelper;
-import com.ags.ayolelang.DBHelper.TawaranHelper;
-import com.ags.ayolelang.Models.Lelang;
-import com.ags.ayolelang.Models.Tawaran;
 import com.ags.ayolelang.R;
-import com.shuhart.stepview.StepView;
 
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.util.ArrayList;
-import java.util.Locale;
-
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link FragmentPemilihanMitra.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link FragmentPemilihanMitra#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class FragmentPemilihanMitra extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
 
-    private TextView txt_idGarapan;
-    private TextView txt_biayaGarapan;
-    private TextView txt_totalMitra;
-    private TextView txt_subtext_judul;
-    private RecyclerView rv_mitra;
-    private int lelang_id;
-    private ImageButton btn_Menu;
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
-    @Nullable
+    private OnFragmentInteractionListener mListener;
+
+    public FragmentPemilihanMitra() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FragmentPemilihanMitra.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FragmentPemilihanMitra newInstance(String param1, String param2) {
+        FragmentPemilihanMitra fragment = new FragmentPemilihanMitra();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_fragment_pemilihan_mitra, null);
-
-        txt_idGarapan = view.findViewById(R.id.txt_idGarapan);
-        txt_biayaGarapan = view.findViewById(R.id.txt_perkiraanBiaya);
-        txt_totalMitra = view.findViewById(R.id.txt_jumlahMitra);
-        txt_subtext_judul=view.findViewById(R.id.txt_subtext_judul);
-        rv_mitra=view.findViewById(R.id.rv_mitra);
-        btn_Menu=view.findViewById(R.id.btn_Menu);
-        btn_Menu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getActivity().onBackPressed();
-            }
-        });
-
-        Bundle bundle = getArguments();
-        lelang_id=bundle.getInt("lelang_id");
-        loadData();
-        return view;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
 
-    private void loadData() {
-        LelangHelper lelangHelper = new LelangHelper(getActivity());
-        lelangHelper.open();
-        Lelang lelang = lelangHelper.getLelang(lelang_id);
-        lelangHelper.close();
-//        if (lelang.getLelang_judul().length()<=28){
-            txt_subtext_judul.setText(lelang.getLelang_judul());
-//        }else{
-//            txt_subtext_judul.setText(lelang.getLelang_judul().substring(0,28)+"...");
-//        }
-
-        txt_idGarapan.setText(lelang.getLelang_id() + "");
-        txt_biayaGarapan.setText("Rp. " + currencyFormat(lelang.getLelang_anggaran()+""));
-
-        TawaranHelper tawaranHelper = new TawaranHelper(getActivity());
-        tawaranHelper.open();
-        ArrayList<Tawaran> tawarans = tawaranHelper.getlisttawaran(lelang.getLelang_id());
-        tawaranHelper.close();
-        txt_totalMitra.setText(tawarans.size() + "");
-
-        AdapterListProgress_itemMitra adapterListProgressitemMitra_ = new AdapterListProgress_itemMitra(getActivity());
-        adapterListProgressitemMitra_.addItem(tawarans);
-
-        rv_mitra.setLayoutManager(new LinearLayoutManager(getActivity()));
-        rv_mitra.setAdapter(adapterListProgressitemMitra_);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_pemilihan_mitra, container, false);
     }
 
-    private String currencyFormat(String harga) {
-        Locale localeID = new Locale("in", "ID");
-        harga = harga.replaceAll("[.,]", "");
-        DecimalFormat kursIndonesia = (DecimalFormat) DecimalFormat.getCurrencyInstance(localeID);
-        DecimalFormatSymbols formatRp = new DecimalFormatSymbols();
-        formatRp.setCurrencySymbol("");
-        formatRp.setGroupingSeparator('.');
-        formatRp.setMonetaryDecimalSeparator(',');
-        kursIndonesia.setDecimalFormatSymbols(formatRp);
-        return kursIndonesia.format(Double.parseDouble(harga));
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
     }
 }
