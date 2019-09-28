48a8545c0bf22598589d692a19218880dcffad54
Beril <beril.sanda@gmail.com>
2019-09-28 18:14:17 +0700

    push 89 (edit halaman garapan)

--- app/src/main/java/com/ags/ayolelang/Fragment/AccountFragment.java
+++ app/src/main/java/com/ags/ayolelang/Fragment/AccountFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ags.ayolelang.Activity.EditPassword;
@@ -20,32 +21,46 @@ import com.ags.ayolelang.Models.User;
 import com.ags.ayolelang.R;
 import com.ags.ayolelang.Storage.SharedPrefManager;
 
-public class AccountFragment extends Fragment implements View.OnClickListener{
+public class AccountFragment extends Fragment {
 
     View v;
+    TextView logout_btn;
     private TextView acc_Name,acc_email,acc_noTelp;
+    private LinearLayout btn_editProfil, btn_editPassword;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.fragment_account, container,false);
+        logout_btn = v.findViewById(R.id.txt_Logout);
         acc_Name=v.findViewById(R.id.acc_Name);
         acc_email=v.findViewById(R.id.acc_email);
         acc_noTelp=v.findViewById(R.id.acc_noTelp);
+        btn_editProfil=v.findViewById(R.id.btn_Profil);
+        btn_editPassword=v.findViewById(R.id.btn_Password);
         loadAccount();
-
-        v.findViewById(R.id.btn_nextArrow_notifikasi).setOnClickListener(this);
-        v.findViewById(R.id.btn_nextArrow_postingGarapan).setOnClickListener(this);
-        v.findViewById(R.id.btn_nextArrow_riwayatGarapan).setOnClickListener(this);
-        v.findViewById(R.id.btn_nextArrow_gantiBahasa).setOnClickListener(this);
-        v.findViewById(R.id.btn_nextArrow_editProfil).setOnClickListener(this);
-        v.findViewById(R.id.btn_nextArrow_ubahPassword).setOnClickListener(this);
-        v.findViewById(R.id.btn_nextArrow_pusatBantuan).setOnClickListener(this);
-        v.findViewById(R.id.btn_nextArrow_tentangAplikasi).setOnClickListener(this);
-        v.findViewById(R.id.txt_Logout).setOnClickListener(this);
+        logout_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+        btn_editProfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),EditProfile.class));
+            }
+        });
 
         Toolbar toolbar = v.findViewById(R.id.toolbar);
         ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        btn_editPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), EditPassword.class));
+            }
+        });
         return v;
     }
 
@@ -69,32 +84,5 @@ public class AccountFragment extends Fragment implements View.OnClickListener{
         Intent intent = new Intent(getActivity(), LoginActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btn_nextArrow_notifikasi:
-                break;
-            case R.id.btn_nextArrow_postingGarapan:
-                break;
-            case R.id.btn_nextArrow_riwayatGarapan:
-                break;
-            case R.id.btn_nextArrow_gantiBahasa:
-                break;
-            case R.id.btn_nextArrow_editProfil:
-                startActivity(new Intent(getActivity(),EditProfile.class));
-                break;
-            case R.id.btn_nextArrow_ubahPassword:
-                startActivity(new Intent(getActivity(), EditPassword.class));
-                break;
-            case R.id.btn_nextArrow_pusatBantuan:
-                break;
-            case R.id.btn_nextArrow_tentangAplikasi:
-                break;
-            case R.id.txt_Logout:
-                logout();
-                break;
-        }
     }
 }
